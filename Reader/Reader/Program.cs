5d3b80de97cdbcb484b82994c88e96fb6a9993e0
Oleg <shilnikovandoleg@gmail.com>
2019-12-26 23:36:53 +0600

    ex

---
+++ Reader/Reader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader
+{
+    public class ReaderData
+    {
+        struct Coordinates
+        {
+            public decimal x;
+            public decimal y;
+            public decimal z;
+            public decimal v;
+            public Coordinates(string[] ads)
+            {
+                x = Convert.ToDecimal(ads[0]);
+                y = Convert.ToDecimal(ads[1]);
+                z = Convert.ToDecimal(ads[2]);
+                v = Convert.ToDecimal(ads[3]);
+            }
+            public void Show()
+            {
+                Console.WriteLine("X: {0},{1}  Y: {2},{3} ", x, y, z, v);
+            }
+        }
+        public void Act()
+        {
+            Coordinates[] array = new Coordinates[3];
+            for (int i = 0; i < 3; i++)
+            {
+                array[i] = new Coordinates(Console.ReadLine().Split('.', ','));
+            }
+            foreach (Coordinates s in array)
+                s.Show();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReaderData a = new ReaderData { };
+            a.Act();
+        }
+    }
+}
---
+++ Reader/Reader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Reader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Reader")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3006eb92-16ba-478d-8c24-4679262c7a74")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
